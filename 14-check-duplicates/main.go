2f195c9a8fec53e0f2657a703e499f0749f048e6
AkhileshNS <nsakhilesh02@gmail.com>
2020-07-20 22:20:55 +0530

    Solved problem 14 BEAST MODE

--- 14-check-duplicates/main.go
+++ 14-check-duplicates/main.go
@@ -7,6 +7,7 @@ import (
 	"time"
 )
 
+// O(nlog(n)) : O(1)
 func myFunction(arr []int) int {
 	start := 1          // range starts from 1 to (n - 1)
 	end := len(arr) - 1 // where n is length of the array
@@ -35,11 +36,42 @@ func myFunction(arr []int) int {
 	return start
 }
 
+// O(n) : O(1)
+func checkDuplicates(arr []int) int {
+	curr := arr[len(arr)-1]
+
+	for i := 0; i < len(arr); i++ {
+		curr = arr[curr-1]
+	}
+
+	newCurr := arr[curr-1]
+	size := 1
+
+	for newCurr != curr {
+		newCurr = arr[newCurr-1]
+		size++
+	}
+
+	curr = arr[len(arr)-1]
+	follower := arr[len(arr)-1]
+	for i := 0; i < len(arr); i++ {
+		if i >= size {
+			follower = arr[follower-1]
+		}
+		curr = arr[curr-1]
+		if follower == curr {
+			return follower
+		}
+	}
+
+	return -1
+}
+
 func main() {
-	nums := []int{1, 9, 8, 7, 6, 6, 5, 4, 3, 2}
+	nums := []int{4, 3, 1, 1, 4}
 	rand.Seed(time.Now().UnixNano())
 	rand.Shuffle(len(nums), func(i, j int) { nums[i], nums[j] = nums[j], nums[i] })
 
 	fmt.Println(nums)
-	fmt.Println(myFunction(nums))
+	fmt.Println(checkDuplicates(nums))
 }
