78eb931ab91fe531aba879b54af7cb22566c8835
AkhileshNS <nsakhilesh02@gmail.com>
2020-07-22 23:04:44 +0530

    Solved problem 22 (pending review)

---
+++ 22-coin-combos/main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Recursive
+/* func getCombos(res *map[string][]int, total int, coins, combo []int, cache *map[string]bool) {
+	currTotal := 0
+	comboStr := ""
+	for i, count := range combo {
+		currTotal += (coins[i] * count)
+		comboStr += strconv.Itoa(count)
+	}
+	(*cache)[comboStr] = true
+	if currTotal == total {
+		(*res)[comboStr] = combo
+		return
+	} else if currTotal > total {
+		return
+	}
+
+	for i := range coins {
+		newCombo := make([]int, len(combo))
+		copy(newCombo, combo)
+		newCombo[i]++
+		newComboStr := ""
+		for _, count := range newCombo {
+			newComboStr += strconv.Itoa(count)
+        }
+        if
+		getCombos(res, total, coins, newCombo, cache)
+	}
+}
+
+func getCoins(coins []int, total int) []string {
+	res := map[string][]int{}
+	combo := []int{}
+
+	for i := 0; i < len(coins); i++ {
+		combo = append(combo, 0)
+	}
+
+	getCombos(&res, total, coins, combo, &map[string]bool{})
+	converted := []string{}
+
+	for _, val := range res {
+		str := ""
+
+		for i, count := range val {
+			for j := 0; j < count; j++ {
+				str += strconv.Itoa(coins[i]) + "+"
+			}
+		}
+
+		if len(str) > 0 {
+			converted = append(converted, str[:len(str)-1])
+		}
+	}
+
+	return converted
+} */
+
+func getCoins(coins []int, total int) int {
+	// Each index in ways represents the amount to reach
+	// The value at each index represents the number of ways to reach (index) amount
+	ways := []int{}
+	for i := 0; i <= total; i++ {
+		ways = append(ways, 0)
+	}
+	ways[0] = 1
+
+	for _, coin := range coins {
+		for amount := coin; amount <= total; amount++ {
+			remainder := amount - coin
+			ways[amount] += ways[remainder]
+		}
+	}
+
+	return ways[total]
+}
+
+func main() {
+	denominations := []int{2, 3, 4}
+	total := 7
+	fmt.Println(getCoins(denominations, total))
+}
